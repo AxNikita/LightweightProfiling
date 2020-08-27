77fffa02cdadb7be7b64a5f5425453fac4c53eb7
AxNikita <51452797+AxNikita@users.noreply.github.com>
2020-08-27 22:32:27 +0600

    + Init Commit

---
+++ src/main/java/com/axproject/lightweightprofiling/Application.java
@@ -0,0 +1,10 @@
+package com.axproject.lightweightprofiling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
